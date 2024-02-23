f72f39ff48462d8737da8e360177d854d5b4d14c
JustinLieu06 <justinlieu06@gmail.com>
2024-02-23 14:18:05 -0800

    export list helper

--- tests/reverse.test.js
+++ tests/reverse.test.js
@@ -1,28 +1,29 @@
-const { test } = require('node:test')
-const assert = require('node:assert')
+// const { test } = require('node:test')
+// const assert = require('node:assert')
 
-const reverse = require('../utils/for_testing').reverse
+// const reverse = require('../utils/for_testing').reverse
 
-test('reverse of a', () => {
-  const result = reverse('a')
+// test('reverse of a', () => {
+//   const result = reverse('a')
 
-  assert.strictEqual(result, 'a')
-})
+//   assert.strictEqual(result, 'a')
+// })
 
-test('reverse of react', () => {
-  const result = reverse('react')
+// test('reverse of react', () => {
+//   const result = reverse('react')
 
-  assert.strictEqual(result, 'tcaer')
-})
+//   assert.strictEqual(result, 'tcaer')
+// })
 
-test('reverse of saippuakauppias', () => {
-  const result = reverse('saippuakauppias')
+// test('reverse of saippuakauppias', () => {
+//   const result = reverse('saippuakauppias')
 
-  assert.strictEqual(result, 'saippuakauppias')
-})
+//   assert.strictEqual(result, 'saippuakauppias')
+// })
 
-test('reverse of react', () => {
-    const result = reverse('react')
+// test('reverse of react', () => {
+//     const result = reverse('react')
 
-    assert.strictEqual(result, 'tkaer')
-})
+//     // assert.strictEqual(result, 'tkaer') // intentional fail
+//     assert.strictEqual(result, 'tcaer')
+// })
