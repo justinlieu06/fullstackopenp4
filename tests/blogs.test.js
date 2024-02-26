2a5e554644836e5287070b7c00c6908b557b2bbf
JustinLieu06 <justinlieu06@gmail.com>
2024-02-25 23:14:50 -0800

    blog test

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -3,3 +3,14 @@ const supertest = require("supertest");
 const app = require("../app");
 
 const api = supertest(app);
+
+test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/)
+      .expect((res) => {
+        console.log(typeof res.body, res.body);
+        expect(res.body).toHaveLength(2);
+      });
+  });
