dbd31ea94019eb9751987afea987d92343649e97
JustinLieu06 <justinlieu06@gmail.com>
2024-03-04 15:23:28 -0800

    additional blog

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -19,6 +19,13 @@ test('there are two blogs', async () => {
     assert.strictEqual(response.body.length, 2)
 })
 
+test('the first blog is about HTTP methods', async () => {
+    const response = await api.get('/api/blogs')
+
+    const contents = response.body.map(e => e.content)
+    assert.strictEqual(contents.includes('HTML is easy'), true)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
