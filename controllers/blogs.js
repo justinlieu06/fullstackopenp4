90998f07ca02fe061a04a87e74563ed00915922e
JustinLieu06 <justinlieu06@gmail.com>
2024-03-07 14:03:29 -0800

    async await ended to http requests

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,20 +1,33 @@
 const express = require("express");
+const { response } = require("../app");
 const { Blog } = require("../models/blogs");
 
 const blogsRouter = express.Router();
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
+// blogsRouter.get("/", (request, response) => {
+//   Blog.find({}).then((blogs) => {
+//     response.json(blogs);
+//   });
+// });
+
+blogsRouter.get("/", async(request, response) => {
+    const blogs = await Blog.find({});
     response.json(blogs);
-  });
-});
+})
 
-blogsRouter.post("/", (request, response) => {
-  const blog = new Blog(request.body);
+// blogsRouter.post("/", (request, response) => {
+//   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
+//   blog.save().then((result) => {
+//     response.status(201).json(result);
+//   });
+// });
+
+blogsRouter.post("/", async(request, response) => {
+    const blogs = new Blog(request.body);
+
+    await blogs.save();
     response.status(201).json(result);
-  });
-});
+})
 
 exports.blogsRouter = blogsRouter;
