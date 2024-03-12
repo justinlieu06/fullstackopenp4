bb7f3fd7fdce24b187d833cf556ba9df10632486
JustinLieu06 <justinlieu06@gmail.com>
2024-03-12 14:49:32 -0700

    added blog routers

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -30,4 +30,25 @@ blogsRouter.post("/", async(request, response) => {
     response.status(201).json(result);
 })
 
+blogsRouter.post('/', async (request, response) => {
+    const body = request.body
+
+    const blog = new Blog({
+        content: body.content,
+        important: body.important || false,
+    })
+
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+})
+  
+blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+        response.json(blog)
+    } else {
+        response.status(404).end()
+    }
+})
+
 exports.blogsRouter = blogsRouter;
